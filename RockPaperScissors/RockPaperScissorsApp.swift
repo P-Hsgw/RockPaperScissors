ca3e16d167458a32144e07286a582da793ee23fb
Patryk Hasegawa <contact@p-hsgw.com>
2022-03-01 22:45:24 +0900

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Patryk Hasegawa on 2022/03/01.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
