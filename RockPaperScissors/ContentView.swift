5a047923bd4e5253f5fbb17ba082bcb1f379e060
Patryk Hasegawa <contact@p-hsgw.com>
2022-03-02 10:39:19 +0900

    Add basic logic of the game, display the result

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -9,24 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var moves = ["rock", "paper", "scissors"]
-    @State private var currentChoice: String = ""
+    @State private var playerChoice: String = ""
+    @State private var computerChoice: String = ""
     @State private var shouldWin: Bool?
+    @State private var currentMessage:String = ""
     
     func makePlayerChoice(choice: String) {
-        currentChoice = choice.uppercased()
+        playerChoice = choice
     }
     
     func makeComputerChoice() {
-        let randomChoice = Int.random(in: 1...2)
-        var choice: Bool?
+        let randomChoice = Int.random(in: 1...3)
+        var choice: String = ""
         if randomChoice == 1 {
-            choice = false
+            choice = "rock"
         } else if randomChoice == 2{
-            choice = true
+            choice = "paper"
+        } else if randomChoice == 3 {
+            choice = "scissors"
         }
-        shouldWin = choice
+        computerChoice = choice
     }
     
+    func checkResult() {
+        if computerChoice == playerChoice {
+            currentMessage = "It's a tie"
+        } else if computerChoice == "rock" && playerChoice == "scissors" || computerChoice == "paper" && playerChoice == "rock" || computerChoice == "scissors" && playerChoice == "paper" {
+            currentMessage = "Computer wins this time"
+        } else {
+            currentMessage = "You won this time"
+        }
+    }
     
     
     var body: some View {
@@ -37,16 +50,21 @@ struct ContentView: View {
                         Button {
                             makePlayerChoice(choice: "rock")
                             makeComputerChoice()
+                            checkResult()
                         } label: {
                             Text("Rock")
                         }.buttonStyle(.bordered)
                         Button {
                             makePlayerChoice(choice: "paper")
+                            makeComputerChoice()
+                            checkResult()
                         } label: {
                             Text("Paper")
                         }.buttonStyle(.bordered)
                         Button {
                             makePlayerChoice(choice: "scissors")
+                            makeComputerChoice()
+                            checkResult()
                         } label: {
                             Text("Scissors")
                         }.buttonStyle(.bordered)
@@ -54,7 +72,7 @@ struct ContentView: View {
                 }header: {
                     Text("Chose your destiny")
                 }
-                Text(currentChoice)
+                Text(currentMessage)
                     .padding()
                 Spacer()
                 
